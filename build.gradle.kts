23cf4ddd1e9b024e94b7d010f99f4cbe5e02e2c1
Yu, Nan <aaron@live.com>
2021-02-01 11:36:10 +0800

    Upgrade springboot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.4.RELEASE"
-	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.3.61"
-	kotlin("plugin.spring") version "1.3.61"
+	id("org.springframework.boot") version "2.4.2"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.21"
+	kotlin("plugin.spring") version "1.4.21"
 }
 
 group = "org.mad"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_15
 
 repositories {
 	mavenCentral()
@@ -20,18 +20,16 @@ dependencies {
 	implementation("org.springframework:spring-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "15"
 	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
--- src/main/kotlin/org/mad/dojob/DojobApplication.kt
+++ src/main/kotlin/org/mad/dojob/DojobApplication.kt
@@ -1,17 +1,11 @@
 package org.mad.dojob
 
-import org.mad.dojob.item.Job
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DojobApplication(private val job: Job) : CommandLineRunner {
-    override fun run(vararg args: String?) {
-        job.doJob()
-    }
-}
+class DojobApplication
 
 fun main(args: Array<String>) {
-    runApplication<DojobApplication>(*args)
+	runApplication<DojobApplication>(*args)
 }
